f550a65f666c40a6db9a1885d39767b57542383d
polycarp <polycarproyal@gmail.com>
2020-09-24 14:38:03 +0300

    update

--- src/app/guards/intro.guard.ts
+++ src/app/guards/intro.guard.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Plugins } from '@capacitor/core';
 import { Observable } from 'rxjs';
 
+const  { Storage } = Plugins;
+
+export const INTRO_KEY = 'intro-seen';
 @Injectable({
   providedIn: 'root'
 })
 export class IntroGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+
+  constructor(private router: Router) { }
+async canLoad(): Promise<boolean> {
+  const hasSeenIntro = await Storage.get({ key: INTRO_KEY });
+
+  if (hasSeenIntro && (hasSeenIntro.value === 'true')) {
     return true;
+  } else {
+    this.router.navigateByUrl('/intro', { replaceUrl:true });
+    return false;
   }
+}
 }
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,7 +4,7 @@ import { TabsPage } from "./tabs.page";
 
 const routes: Routes = [
   {
-    path: "tab",
+    path: "",
     component: TabsPage,
     children: [
       {
@@ -22,17 +22,12 @@ const routes: Routes = [
         loadChildren: () =>
           import("../tab3/tab3.module").then((m) => m.Tab3PageModule),
       },
-      {
-        path: "",
-        redirectTo: "/tabs/tab1",
-        pathMatch: "full",
-      },
-    ],
-  },
   {
     path: "",
     redirectTo: "/tabs/tab1",
     pathMatch: "full",
+  }
+    ]
   },
 ];
 
